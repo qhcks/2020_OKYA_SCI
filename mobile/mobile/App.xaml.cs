19673424fd74289a55c0cca12e99d89d45ecba90
김보찬 <william0407@naver.com>
2020-08-05 13:32:40 +0900

    초기 커밋.

---
+++ mobile/mobile/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("hello");
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
