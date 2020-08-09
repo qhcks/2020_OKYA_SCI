0b68acfdc36b623892d213fe5426102109e488dc
김보찬 <william0407@naver.com>
2020-08-09 22:02:07 +0900

    add new project

---
+++ App1/App1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using App1.ViewModels;
+
+namespace App1.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
